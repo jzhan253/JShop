bf9f9b842820ac09cf83e3f409e2bceaa27ae725
Jinfan <kimthebestman@gmail.com>
2021-09-12 16:10:59 -0700

    Spring initializr

---
+++ src/main/java/com/jincreation/JShop/JShopApplication.java
@@ -0,0 +1,13 @@
+package com.jincreation.JShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JShopApplication.class, args);
+	}
+
+}
